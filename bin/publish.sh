70e6f0ea6ffea9519cb0a6de15dee549f8c5199e
Ryan Tam <ryantam626@gmail.com>
2021-01-16 12:24:12 +0000

    Upgrade publish script

--- bin/publish.sh
+++ bin/publish.sh
@@ -3,5 +3,9 @@
 source bin/pre-publish-check.sh
 cd $LABEXTENSION_PATH
 npm publish --access public
+python setup.py sdist
+twine upload dist/jupyterlab_sublime-"${PUBLISH_VERSION/v/}".tar.gz
+python setup.py bdist_wheel
+twine upload dist/jupyterlab_sublime-"${PUBLISH_VERSION/v/}"-py3-none-any.whl
 git tag -a "${PUBLISH_VERSION}" -m "${PUBLISH_VERSION}"
 git push origin --tags
